66ae98dbe50f6c5c5413624b6550cfa1121e3bbb
Batista Alvarez Natanael <natanael.batista@upr.edu>
2023-08-19 01:38:15 -0400

    Initial Commit

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 from website import create_app
 from flask import Flask
+from waitress import serve
 
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    serve(app, host="0.0.0.0", port=5000)
     
