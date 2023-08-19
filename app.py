e1cd748a5160f12a0b621271c1c5f26ae987686d
Batista Alvarez Natanael <natanael.batista@upr.edu>
2023-08-19 01:58:46 -0400

    requ

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from website import create_app
 from flask import Flask
 from waitress import serve
+import os
 
 app = create_app()
 
-if __name__ == '__main__':
-    serve(app, host="0.0.0.0", port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    serve(app, host="0.0.0.0", port=port)
     
